c57b7067df2c4d85c4ecb2c08dfd5732f592532a
KEKAMACHADO <erikamachado@hotmail.com>
2024-10-22 22:17:17 -0500

    creacion slice thunk api dogs

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { catReducer } from "./slice/catSlice";
+import { dogReducer } from "./slice/dogSlice";
 
 const store = configureStore({
     reducer: {
         cats: catReducer,
+        dogs: dogReducer
     }
 })
 export default store
