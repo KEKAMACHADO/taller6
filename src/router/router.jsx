2f221691108df60a99c175c20b59056e67322138
KEKAMACHADO <erikamachado@hotmail.com>
2024-10-22 22:29:32 -0500

    creacion estilos cart y importacion de paginas al router

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Home from "../pages/Home";
+import CatList from "../pages/CatList";
+import DogList from "../pages/DogList";
 
 function AppRouter() {
     return( 
